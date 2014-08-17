076a8c83f7c053229dc90b233fa820d5137c41f7
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2014-08-17 15:45:58 -0700

    Update cache.pp

--- manifests/cache.pp
+++ manifests/cache.pp
@@ -1,9 +1,11 @@
 define carbon::cache(
+  $prefix = '/opt/graphite'
 ) {
 
   concat::fragment { "cache_${title}":
     target  => "${prefix}/conf/carbon.conf",
     content => template('carbon/cache.erb'),
+    order   => 10,
   }
 
 }
