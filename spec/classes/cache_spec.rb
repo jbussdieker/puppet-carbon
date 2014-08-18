76554d87a212bd745195d216b8d7bc92a5c861f2
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-08-18 15:32:10 -0700

    Make cache a class for now

---
+++ spec/classes/cache_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe 'carbon::cache' do
+  let (:thing) { "/var/lib/puppet/concat/_opt_graphite_conf_carbon.conf/fragments/10_cache" }
+
+  describe "carbon.conf template content" do
+    [
+      {
+        :title => 'should set max_creates_per_minute',
+        :attr  => 'max_creates_per_minute',
+        :value => '50',
+        :match => 'MAX_CREATES_PER_MINUTE = 50',
+      },
+      {
+        :title => 'should set line_receiver_interface',
+        :attr  => 'line_receiver_interface',
+        :value => '0.0.0.0',
+        :match => 'LINE_RECEIVER_INTERFACE = 0.0.0.0',
+      },
+      {
+        :title => 'should set line_receiver_port',
+        :attr  => 'line_receiver_port',
+        :value => '2004',
+        :match => 'LINE_RECEIVER_PORT = 2004',
+      },
+    ].each do |param|
+      context "when #{param[:attr]} is #{param[:value]}" do
+        let :params do { param[:attr].to_sym => param[:value] } end
+
+        it { should contain_file(thing).with_mode('0640') }
+        it param[:title] do
+          verify_contents(subject, thing, Array(param[:match]))
+          Array(param[:notmatch]).each do |item|
+            should contain_file(thing).without_content(item)
+          end
+        end
+      end
+    end
+  end
+
+  it { should contain_file('/etc/init/carbon-cache.conf') }
+  it { should contain_service('carbon-cache') }
+end
