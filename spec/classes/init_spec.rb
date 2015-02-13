741f76fd170a5297813163e98cb1b3f852fc0f4f
Joshua B. Bussdieker <jbussdieker@brightroll.com>
2015-02-13 15:33:41 -0800

    Bump gems and remove all but one test for now

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe 'carbon' do
   it 'should contain stuff' do
-    #puts subject.resources.to_yaml
-    #verify_contents(subject, "/opt/graphite/conf/carbon.conf", ["ting"])
+    $stderr.puts subject.resources.to_yaml
+    verify_contents(
+      subject,
+      "/var/lib/puppet/concat/_opt_graphite_conf_storage-schemas.conf/fragments/99_default_1min_for_1day",
+      ["[default_1min_for_1day]"]
+    )
   end
 end
