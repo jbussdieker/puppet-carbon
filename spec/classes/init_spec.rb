55cbcf72ee9ce63105dc7ad9825581a6be035e3f
Joshua B. Bussdieker <jbussdieker@brightroll.com>
2015-02-13 15:51:05 -0800

    Call it

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe 'carbon' do
   it 'should contain stuff' do
-    $stderr.puts subject.resources.to_yaml
+    $stderr.puts subject.call.to_yaml
     verify_contents(
-      subject,
+      subject.call,
       "/var/lib/puppet/concat/_opt_graphite_conf_storage-schemas.conf/fragments/99_default_1min_for_1day",
       ["[default_1min_for_1day]"]
     )
