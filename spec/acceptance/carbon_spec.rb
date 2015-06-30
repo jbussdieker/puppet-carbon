85309d4a091259aa37766e79d8ba64558373e37d
Joshua B. Bussdieker <jbussdieker@gmail.com>
2015-06-29 17:39:50 -0700

    Speed up tests

--- spec/acceptance/carbon_spec.rb
+++ spec/acceptance/carbon_spec.rb
@@ -105,28 +105,23 @@ describe 'carbon class' do
       apply_manifest(manifest, :catch_failures => true)
       expect(apply_manifest(manifest, :catch_changes => true).exit_code).to be_zero
     end
+  end
 
-    describe 'command line tools' do
-      before do
-        apply_manifest(manifest, :catch_failures => true)
-        apply_manifest(manifest, :catch_changes => true)
-      end
+  describe 'command line tools' do
+    it 'carbon-cache.py should work' do
+      shell("#{prefix}/bin/carbon-cache.py --help", :acceptable_exit_codes => 0)
+    end
 
-      it 'carbon-cache.py should work' do
-        shell("#{prefix}/bin/carbon-cache.py --help", :acceptable_exit_codes => 0)
-      end
+    it 'carbon-relay.py should work' do
+      shell("#{prefix}/bin/carbon-relay.py --help", :acceptable_exit_codes => 0)
+    end
 
-      it 'carbon-relay.py should work' do
-        shell("#{prefix}/bin/carbon-relay.py --help", :acceptable_exit_codes => 0)
-      end
+    it 'carbon-aggregator.py should work' do
+      shell("#{prefix}/bin/carbon-aggregator.py --help", :acceptable_exit_codes => 0)
+    end
 
-      it 'carbon-aggregator.py should work' do
-        shell("#{prefix}/bin/carbon-aggregator.py --help", :acceptable_exit_codes => 0)
-      end
-
-      it 'carbon-client.py should work' do
-        shell("#{prefix}/bin/carbon-client.py --help", :acceptable_exit_codes => 0)
-      end
+    it 'carbon-client.py should work' do
+      shell("#{prefix}/bin/carbon-client.py --help", :acceptable_exit_codes => 0)
     end
   end
 end
